2a9ebbce8e6ea987e1d802d813bd7542ff1fba0a
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-12-24 13:46:27 +0200

    add a registration page

---
+++ app/(auth routes)/sign-up/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import css from "./SignUpPage.module.css";
+import { register, RegisterRequest } from "@/lib/api/clientApi";
+
+const SignUpPage = () => {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      const formValues: RegisterRequest = {
+        email: formData.get("email") as string,
+        password: formData.get("password") as string,
+      };
+
+      const user = await register(formValues);
+
+      if (user) {
+        router.push("/profile");
+      }
+    } catch {
+      setError("Registration failed");
+    }
+  };
+
+  return (
+    <main className={css.mainContent}>
+      <h1 className={css.formTitle}>Sign up</h1>
+
+      <form className={css.form} action={handleSubmit}>
+        <div className={css.formGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.actions}>
+          <button type="submit" className={css.submitButton}>
+            Register
+          </button>
+        </div>
+
+        {error && <p className={css.error}>{error}</p>}
+      </form>
+    </main>
+  );
+};
+
+export default SignUpPage;
--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,5 +1,6 @@
 import { CreateNoteData, Note } from "@/types/note";
 import nextServer from "./api";
+import { User } from "@/types/user";
 
 interface FetchNotesResponse {
   notes: Note[];
@@ -31,5 +32,15 @@ export const deleteNote = async (id: string): Promise<Note> => {
 
 export const fetchNoteById = async (id: string): Promise<Note> => {
   const response = await nextServer.get<Note>(`/notes/${id}`);
+  return response.data;
+};
+
+export interface RegisterRequest {
+  email: string;
+  password: string;
+}
+
+export const register = async (data: RegisterRequest): Promise<User> => {
+  const response = await nextServer.post<User>("/auth/register", data);
   return response.data;
 };
